5ace8a2a4f6c8418d76d03f05485b72a74d2e81d
DiamaraJakelineMuñozVargas <diamaramunozvargas@gmail.com>
2026-03-19 11:19:28 -0400

    avance

--- main.go
+++ main.go
@@ -1,87 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"path/filepath"
-
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/gofiber/fiber/v2/middleware/recover"
+	"joseluis244/ProyectoGo/webserver"
 )
 
-func crearEntornoCliente(nombre string) error {
-	// 1. Nombrar carpetasss
-	carpetas := []string{
-		filepath.Join("Symphony", nombre, "DCM"),
-		filepath.Join("Symphony", nombre, "MYSQL"),
-		filepath.Join("Symphony", nombre, "MONGO"),
-		filepath.Join("Symphony", nombre, "INF"),
-		filepath.Join("Symphony", nombre, "KVSTORE"),
-		filepath.Join("MedicareSoft", nombre, "App"),
-	}
-
-	//Crear carpetasddd
-	for _, ruta := range carpetas {
-		if err := os.MkdirAll(ruta, 0755); err != nil {
-			return err
-		}
-	}
-
-	// Plantilla del Docker Compose
-	contenido := fmt.Sprintf(`version: '3.8'
-services:
-  db-%s:
-    image: mysql:8.0
-    container_name: mysql-%s
-    volumes:
-      - ../../Symphony/%s/MYSQL:/var/lib/mysql`, nombre, nombre, nombre)
-
-	// 4. Escribir el archivo
-	rutaArchivo := filepath.Join("MedicareSoft", nombre, "compose.yml")
-	return os.WriteFile(rutaArchivo, []byte(contenido), 0644)
-}
-
 func main() {
-	app := fiber.New()
-
-	// Middlewares
-	app.Use(logger.New())
-	app.Use(recover.New())
-
-	// Ruta 1: Inicio
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Servidor de Gestión MedicareSoft Inicializado 🚀")
-	})
-
-	// Ruta 2
-	app.Get("/health", func(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{"status": "UP"})
-	})
-
-	// NUEVA RUTA: Crear cliente (Ejemplo: localhost:8080/crear/Juan)
-	app.Get("/crear/:nombre", func(c *fiber.Ctx) error {
-		nombre := c.Params("nombre")
-
-		err := crearEntornoCliente(nombre)
-		if err != nil {
-			return c.Status(500).JSON(fiber.Map{
-				"error": "No se pudo crear el entorno",
-			})
-		}
-
-		return c.JSON(fiber.Map{
-			"mensaje": "¡Entorno de " + nombre + " creado con éxito!",
-		})
-	})
-
-	// Configuración del puerto000
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-
-	log.Printf("Iniciando servidor en el puerto %s...", port)
-	log.Fatal(app.Listen(":" + port))
+	webserver.Inicio()
 }
--- webserver/webserver.go
+++ webserver/webserver.go
@@ -1,1 +1,60 @@
 package webserver
+
+import (
+	//"fmt"
+	// "log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	// "github.com/gofiber/fiber/v2/middleware/logger"
+	// "github.com/gofiber/fiber/v2/middleware/recover"
+)
+
+func Inicio() {
+	app := fiber.New()
+	app.Post("/crearcliente/:nombre", func(c *fiber.Ctx) error {
+		nombre := c.Params("nombre")
+		crearEntornoCliente(nombre)
+		plantilla, err := os.ReadFile("compose.template")
+		if err != nil {
+			return c.Status(500).SendString("No se encontro la plantilla")
+		}
+		//lo busca y lo reemplaza
+		nuevoContent := string(plantilla)
+		nuevoContent = strings.ReplaceAll(nuevoContent, "${HOSPITAL_NAME}", nombre)
+
+		// nuevoContent = strings.ReplaceAll(nuevoContent, "${MYSQL_PORT}", "3306")
+		// nuevoContent = strings.ReplaceAll(nuevoContent, "${MONGO_PORT}", "27017")
+		// nuevoContent = strings.ReplaceAll(nuevoContent, "${DICOM_PORT}", "3101")
+		// nuevoContent = strings.ReplaceAll(nuevoContent, "${HTTP_PORT}", "8042")
+		// nuevoContent = strings.ReplaceAll(nuevoContent, "${APP_PORT}", "4000")
+
+		// lo guarda
+		rutadestino := filepath.Join("MedicareSoft", nombre, "compose.yml")
+		err = os.WriteFile(rutadestino, []byte(nuevoContent), 0644)
+		if err != nil {
+			return c.Status(500).SendString("Error al crear el compose.yml")
+		}
+		return c.JSON(fiber.Map{"status": "Creado para el cliente " + nombre})
+	})
+	app.Listen(":8080")
+
+}
+func crearEntornoCliente(nombre string) {
+	carpetas := []string{
+		filepath.Join("Symphony", nombre, "DCM"),
+		filepath.Join("Symphony", nombre, "MYSQL"),
+		filepath.Join("Symphony", nombre, "MONGO"),
+		filepath.Join("Symphony", nombre, "INF"),
+		filepath.Join("Symphony", nombre, "KVSTORE"),
+		filepath.Join("MedicareSoft", nombre, "App"),
+	}
+
+	//Crear carpetasddd
+	for _, ruta := range carpetas {
+		os.MkdirAll(ruta, 0755)
+
+	}
+}
